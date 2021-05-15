5a0a1ac74474aa9796d160edb7276ff7aa383998
Yuta Ide <sadness.ojisan@gmail.com>
2021-05-16 04:40:48 +0900

    success kusalize (#2)
    * add weedize
    
    * log date
    
    * get v
    
    * show gray
    
    * col

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,15 +1,73 @@
+import { weedize } from "weedize";
+
 class Weed365 extends HTMLElement {
-  shadow;
+  shadow: ShadowRoot;
   constructor() {
     super();
-    // 1. HTMLから分離されたノード(ShadowDOM)を作成する。
-    //    これで、親のドキュメントのCSSなどに影響されない環境を作成できます。
+    const start = this.getAttribute("start");
+    if (start === null) throw new Error("should set start");
+
+    const layout = weedize(new Date(start));
+
+    console.log(layout);
+    const kusasString = this.getAttribute("kusas");
+    if (kusasString === null) throw new Error("should set kusasString");
+    const kusas = JSON.parse(kusasString);
+    const values = (Object.values(kusas) as any) as number[]; // TODO: validation
+    const max = values.reduce((a, b) => Math.max(a, b));
+    const kusaLayout = layout.map((week) => {
+      return week.map((dateString) => {
+        if (dateString === undefined) return undefined;
+        const date = dateString as Date;
+        const YYYYMMDD = `${date.getFullYear()}-${(date.getMonth() + 1)
+          .toString()
+          .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+        const value = kusas[YYYYMMDD] ?? 0;
+        return { date: dateString, value: value / max };
+      });
+    });
     this.shadow = this.attachShadow({ mode: "open" });
-    // 2. 今の時間を表示する h1 エレメントを ShadowDOM 下に作成する。
-    const now = new Date();
-    const clockElement = document.createElement("h1");
-    clockElement.textContent = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-    this.shadow.append(clockElement);
+    this.shadow.innerHTML = `
+      <style>
+      .month{
+        display:flex;
+      }
+      .week{
+        display: flex;
+        flex-direction: column;
+      }
+      .day{
+        width: 11px;
+        height: 11px;
+        background: rgb(33 110 57);
+        margin-top: 6px;
+        margin-left: 6px;
+        outline: 1px solid hsl(210deg 13% 12% / 6%);
+      }
+      .empty {
+        background: #ebedf0;
+      }
+      </style>
+      <div class="month">
+        ${kusaLayout
+          .map(
+            (week) =>
+              `<div class="week">
+          ${week
+            .map(
+              (day) =>
+                `<div class="day ${
+                  day === undefined || day.value === 0 ? "empty" : ""
+                }" style="opacity: ${
+                  day === undefined || day.value === 0 ? 1 : day.value
+                };"></div>`
+            )
+            .join(" ")}
+          </div>`
+          )
+          .join(" ")}
+      </div>
+    `;
   }
 }
 
