b4bae4abbf367bdbe088e49725e213939d76be01
sadness_ojisan <sadness.ojisan@gmail.com>
2021-05-16 02:58:09 +0900

    log date

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # weed365
+
 GitHub weed style Web Components
+
+## dev
+
+```sh
+yarn lib run build:dev
+
+open packages/example/index.html
+```
--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,20 +1,20 @@
-// @ts-ignore
 import { weedize } from "weedize";
 
 class Weed365 extends HTMLElement {
-  shadow;
+  shadow: ShadowRoot;
   constructor() {
     const layout = weedize(new Date("2021-01-01"));
-    console.log(layout);
     super();
-    // 1. HTMLから分離されたノード(ShadowDOM)を作成する。
-    //    これで、親のドキュメントのCSSなどに影響されない環境を作成できます。
     this.shadow = this.attachShadow({ mode: "open" });
-    // 2. 今の時間を表示する h1 エレメントを ShadowDOM 下に作成する。
-    const now = new Date();
-    const clockElement = document.createElement("h1");
-    clockElement.textContent = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
-    this.shadow.append(clockElement);
+    this.shadow.innerHTML = `
+      <style></style>
+      <div class="kusas">
+        ${layout.map((week) =>
+          week.map((day) => `<div>${JSON.stringify(day)}</div>`)
+        )}
+      </div>
+    `;
+    console.log(this.attributes);
   }
 }
 
