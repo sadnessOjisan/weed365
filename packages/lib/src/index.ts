f94d765c4f56e34fd7ab4b3ef560d1e7f24ffab8
sadness_ojisan <sadness.ojisan@gmail.com>
2021-05-16 03:41:16 +0900

    get v

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -3,18 +3,38 @@ import { weedize } from "weedize";
 class Weed365 extends HTMLElement {
   shadow: ShadowRoot;
   constructor() {
-    const layout = weedize(new Date("2021-01-01"));
     super();
+    const start = this.getAttribute("start");
+    if (start === null) throw new Error("should set start");
+
+    const layout = weedize(new Date(start));
+
+    console.log(layout);
+    const kusasString = this.getAttribute("kusas");
+    if (kusasString === null) throw new Error("should set kusasString");
+    const kusas = JSON.parse(kusasString);
+    const values = (Object.values(kusas) as any) as number[]; // TODO: validation
+    const max = values.reduce((a, b) => Math.max(a, b));
+    const kusaLayout = layout.map((week) => {
+      return week.map((dateString) => {
+        if (dateString === undefined) return undefined;
+        const date = dateString as Date;
+        const YYYYMMDD = `${date.getFullYear()}-${(date.getMonth() + 1)
+          .toString()
+          .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
+        const value = kusas[YYYYMMDD] ?? 0;
+        return { date: dateString, value: value / max };
+      });
+    });
     this.shadow = this.attachShadow({ mode: "open" });
     this.shadow.innerHTML = `
       <style></style>
       <div class="kusas">
-        ${layout.map((week) =>
+        ${kusaLayout.map((week) =>
           week.map((day) => `<div>${JSON.stringify(day)}</div>`)
         )}
       </div>
     `;
-    console.log(this.attributes);
   }
 }
 
